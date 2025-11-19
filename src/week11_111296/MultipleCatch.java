718a0bd19e25e7cca3ae592aea9c0f0711068d68
Viero <savieroalim@gmail.com>
2025-11-19 11:09:25 +0700

    Multiple Catch

---
+++ src/week11_111296/MultipleCatch.java
@@ -0,0 +1,22 @@
+package week11_111296;
+
+import java.util.Scanner;
+
+public class MultipleCatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		try {
+			int n=Integer.parseInt(sc.nextLine());
+			int result=n/10;
+			System.out.println(result);
+		}catch(ArithmeticException ex) {
+			System.out.println("Arithmetic "+ex);
+		}catch(NumberFormatException ex) {
+			System.out.println("NumberFormatException "+ex);
+		}
+
+	}
+
+}
